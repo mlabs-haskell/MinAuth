9ce7d6a9a4b127c3f631d1ffd709d4dd912d69e0
adamczykm <m@adamczykm.me>
2023-10-16 14:02:47 +0200

    Add documentation for plugin/fp/utils

--- src/library/plugin/fp/utils.ts
+++ src/library/plugin/fp/utils.ts
@@ -5,17 +5,17 @@ import {
   ProofCacheProvider
 } from './pluginLoader';
 import * as expressCore from 'express-serve-static-core';
-import {
-  fromFailableIO,
-  fromPromise,
-  liftZodParseResult
-} from '@utils/fp/TaskEither';
+import { fromFailableIO, liftZodParseResult } from '@utils/fp/TaskEither';
 import * as R from 'fp-ts/Record';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/lib/function';
-import { JsonProof, verify } from 'o1js';
+import { JsonProof } from 'o1js';
 import { ProofKey, tsToFpProofCacheProvider } from './proofCache';
 
+/**
+ *  Curried function. Given a set of plugins, and an express.js app
+ *  install custom routes for each plugin.
+ */
 export const installCustomRoutes =
   (activePlugins: ActivePlugins) =>
   (app: expressCore.Express): TaskEither<string, void> =>
@@ -38,11 +38,12 @@ export const installCustomRoutes =
       TE.asUnit
     );
 
+/**
+ * Curried function. Given set of plugins, plugin identifier and plugin proof
+ * verification inputs - verify proof, and store and return the plugin output.
+ */
 export const verifyProof =
-  (
-    activePlugins: ActivePlugins,
-    proofCacheProvider: ProofCacheProvider
-  ) =>
+  (activePlugins: ActivePlugins, proofCacheProvider: ProofCacheProvider) =>
   (
     proof: JsonProof,
     publicInputArgs: unknown,
@@ -55,33 +56,26 @@ export const verifyProof =
           console.info(`verifying proof using plugin ${pluginName}`)
         )
       ),
+      // lookup an active plugin by name
       TE.bind('pluginInstance', () =>
         TE.fromOption(() => `plugin ${pluginName} not found`)(
           R.lookup(pluginName)(activePlugins)
         )
       ),
-      // Step 1: check that the proof was generated using a certain verification key.
-      TE.tap(({ pluginInstance }) =>
-        pipe(
-          fromPromise(
-            () => verify(proof, pluginInstance.verificationKey),
-            'unable to verify proof'
-          ),
-          TE.tap((valid) =>
-            valid ? TE.right(undefined) : TE.left('invalid proof')
-          )
-        )
-      ),
-      // Step 2: use the plugin to extract the output. The plugin is also responsible
-      // for checking the legitimacy of the public inputs.
+      // Parse public input arguments.
       TE.bind('typedPublicInputArgs', ({ pluginInstance }) =>
         liftZodParseResult(
           pluginInstance.publicInputArgsSchema.safeParse(publicInputArgs)
         )
       ),
+      // Use the plugin to verify the proof and prepare the output
+      // The plugin is also responsible for checking the legitimacy
+      // of the public inputs.
       TE.bind('output', ({ typedPublicInputArgs, pluginInstance }) =>
         pluginInstance.verifyAndGetOutput(typedPublicInputArgs, proof)
       ),
+
+      // Store the output in the cache and get its cache key.
       TE.bind('proofKey', () =>
         pipe(
           proofCacheProvider.__interface_tag == 'fp'
@@ -96,6 +90,8 @@ export const verifyProof =
           )
         )
       ),
+      // Return the output and the cache key.
+      // (`TE.map` in pipe lifts the function to `TaskEither` monad)
       TE.map(({ proofKey, output }) => {
         return {
           proofKey,
