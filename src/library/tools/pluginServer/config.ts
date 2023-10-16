0566c33416be3dda9f01a57b266d90728feea9ff
Hongrui Fang <chfanghr@gmail.com>
2023-10-16 19:44:13 +0800

    don't fill leaves in default config

--- src/library/tools/pluginServer/config.ts
+++ src/library/tools/pluginServer/config.ts
@@ -2,7 +2,6 @@ import { z } from 'zod';
 import * as PL from '@lib/plugin/fp/pluginLoader';
 import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
-import { Field, Poseidon } from 'o1js';
 
 export const configurationSchema = PL.configurationSchema.extend({
   address: z.string().default('127.0.0.1'),
@@ -19,12 +18,7 @@ export const defaultConfiguration: Configuration = configurationSchema.parse({
     SimplePreimagePlugin: {
       path: mkPluginDir('simplePreimage'),
       config: {
-        roles: {
-          '7555220006856562833147743033256142154591945963958408607501861037584894828141':
-            'admin',
-          '21684304481040958849270710845151658168046794458221536315647897641876555971838':
-            'member'
-        }
+        loadRolesFrom: './.fixtures/roles.json'
       }
     },
     MerkleMembershipsPlugin: {
@@ -32,26 +26,10 @@ export const defaultConfiguration: Configuration = configurationSchema.parse({
       config: {
         trees: [
           {
-            offchainStoragePath: './.fixtures/tree1.json',
-            initialLeaves: (() => {
-              const r: Record<string, string> = {};
-
-              for (let i = 0; i < 100; i++)
-                r[i.toString()] = Poseidon.hash([Field.from(i)]).toString();
-
-              return r;
-            })()
+            offchainStoragePath: './.fixtures/tree1.json'
           },
           {
-            offchainStoragePath: './.fixtures/tree2.json',
-            initialLeaves: (() => {
-              const r: Record<string, string> = {};
-
-              for (let i = 1; i < 100; i *= 2)
-                r[i.toString()] = Poseidon.hash([Field.from(i)]).toString();
-
-              return r;
-            })()
+            offchainStoragePath: './.fixtures/tree2.json'
           }
         ]
       }
