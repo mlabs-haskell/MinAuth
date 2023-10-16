d0c46d7ca9abe3595d4d2ea8c8d78d78cda2747b
adamczykm <m@adamczykm.me>
2023-10-16 14:19:58 +0200

    Add documentation for plugin/index

--- src/library/plugin/index.ts
+++ src/library/plugin/index.ts
@@ -21,20 +21,34 @@ export {
   ProofKey
 } from './fp/proofCache';
 
+/** Read the configuration from a file
+ *  The file path can be passed from $MINAUTH_CONFIG env var and
+ *  defaults to `config.yaml` in the current working directory
+ */
 export const readConfiguration = (cfgPath?: string): Promise<Configuration> =>
   launchTE(fpPluginLoader.readConfiguration(cfgPath));
 
+/**
+ * Given plugin configuration and a proof cache provider, initialize the plugins.
+ */
 export const initializePlugins = (
   cfg: Configuration,
   proofCacheProvider: fpPluginLoader.ProofCacheProvider
 ): Promise<ActivePlugins> =>
   launchTE(fpPluginLoader.initializePlugins(cfg, proofCacheProvider));
 
+/**
+ *  Install custom routes for each active plugin.
+ */
 export const installCustomRoutes = (
   activePlugins: ActivePlugins,
   app: expressCore.Express
 ): Promise<void> => launchTE(fpUtils.installCustomRoutes(activePlugins)(app));
 
+/**
+ * Given set of plugins, plugin identifier and plugin proof * verification inputs
+ * - verify proof, and store and return the plugin output.
+ */
 export const verifyProof = (
   activePlugins: ActivePlugins,
   proofCacheProvider: fpPluginLoader.ProofCacheProvider,
@@ -49,3 +63,7 @@ export const verifyProof = (
       pluginName
     )
   );
+
+// TODO DEV NOTE:
+// the lib/plugin modules should be required to build a plugin.
+// The stuff that is needed to operate them should be exported from the server library.
