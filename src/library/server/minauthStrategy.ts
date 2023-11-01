82894c1389295e0733d277a53fad0fcfe16a7987
adamczykm <m@adamczykm.me>
2023-11-01 14:18:56 +0100

    Add documentation for library server modules

--- src/library/server/minauthStrategy.ts
+++ src/library/server/minauthStrategy.ts
@@ -1,14 +1,21 @@
+// TODO use logger
 import axios from 'axios';
 import { Request } from 'express';
 import { Strategy } from 'passport-strategy';
 import { JsonProof } from 'o1js';
 
+/**
+ * The generic proof shape that can be verified by the plugin server.
+ */
 export interface MinAuthProof {
   plugin: string;
   publicInputArgs: unknown;
   proof: JsonProof;
 }
 
+/**
+ * A result of a proof verification.
+ */
 type VerificationResult =
   | {
       __tag: 'success';
@@ -19,12 +26,15 @@ type VerificationResult =
       error: string;
     };
 
+/**
+ * Forward proof verification to a remote verifier.
+ */
 const verifyProof = (
-  proverUrl: string,
+  verifierUrl: string,
   data: MinAuthProof
 ): Promise<VerificationResult> => {
   console.log('Calling for proof verification with:', data);
-  return axios.post(proverUrl, data).then(
+  return axios.post(verifierUrl, data).then(
     (resp) => {
       if (resp.status == 200) {
         console.log('Received response:', resp);
@@ -48,20 +58,27 @@ export type AuthenticationResponse = {
   output: unknown;
 };
 
+/**
+ * Minauth's integration with passport.js.
+ * This implementation uses the plugin server to verify the proof.
+ */
 class MinAuthStrategy extends Strategy {
   name = 'MinAuthStrategy';
 
   readonly verifyProof: (_: MinAuthProof) => Promise<VerificationResult>;
 
-  public constructor(proverUrl: string = 'http://127.0.0.1:3001/verifyProof') {
+  public constructor(
+    verifierUrl: string = 'http://127.0.0.1:3001/verifyProof'
+  ) {
     super();
 
-    this.verifyProof = (data) => verifyProof(proverUrl, data);
+    this.verifyProof = (data) => verifyProof(verifierUrl, data);
   }
 
   async authenticate(req: Request): Promise<void> {
     console.log('authenticating (strategy) with req:', req.body);
     const loginData = req.body as MinAuthProof; // TODO validate the body
+    // forward the proof verification to the plugin server
     const result = await this.verifyProof(loginData);
 
     if (result.__tag == 'success') {
