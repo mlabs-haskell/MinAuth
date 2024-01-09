2542e62ddb04fa1f4922b6f31644c7d2368d8a0e
adamczykm <m@adamczykm.me>
2024-01-09 21:30:41 +0100

    Enable MM's e2e tests.

--- minauth-demo/minauth-demo-server/src/tests/e2e.test.ts
+++ minauth-demo/minauth-demo-server/src/tests/e2e.test.ts
@@ -1,4 +1,4 @@
-// import * as O from 'fp-ts/lib/Option.js';
+import * as O from 'fp-ts/lib/Option.js';
 import { Field, Poseidon } from 'o1js';
 
 import { runTestGroup } from './test-helper.js';
@@ -20,37 +20,37 @@ runTestGroup({
             return m;
           })()
         }
+      },
+      MerkleMembershipsPlugin: {
+        kind: 'merkle-memberships',
+        config: {
+          feePayer: O.none,
+          trees: [
+            {
+              tree: ((): Map<number, Field> => {
+                const m = new Map();
+
+                for (let i = 0; i < 99; i++)
+                  m.set(i, Poseidon.hash([Field.from(i)]));
+
+                return m;
+              })(),
+              contract: O.none
+            },
+            {
+              tree: ((): Map<number, Field> => {
+                const m = new Map();
+
+                for (let i = 1; i < 99; i *= 2)
+                  m.set(i, Poseidon.hash([Field.from(i)]));
+
+                return m;
+              })(),
+              contract: O.none
+            }
+          ]
+        }
       }
-      // MerkleMembershipsPlugin: {
-      //   kind: 'merkle-memberships',
-      //   config: {
-      //     feePayer: O.none,
-      //     trees: [
-      //       {
-      //         tree: ((): Map<number, Field> => {
-      //           const m = new Map();
-
-      //           for (let i = 0; i < 99; i++)
-      //             m.set(i, Poseidon.hash([Field.from(i)]));
-
-      //           return m;
-      //         })(),
-      //         contract: O.none
-      //       },
-      //       {
-      //         tree: ((): Map<number, Field> => {
-      //           const m = new Map();
-
-      //           for (let i = 1; i < 99; i *= 2)
-      //             m.set(i, Poseidon.hash([Field.from(i)]));
-
-      //           return m;
-      //         })(),
-      //         contract: O.none
-      //       }
-      //     ]
-      //   }
-      // }
     }
   },
   tests: [
@@ -84,32 +84,32 @@ runTestGroup({
         serverUrl: 'http://localhost:3000',
         pluginName: 'SimplePreimagePlugin'
       }
+    },
+    {
+      name: 'good memberships',
+      outcome: {
+        shouldSuccess: true
+      },
+      kind: 'merkleMemberships',
+      config: {
+        pluginUrl: 'http://127.0.0.1:3001/plugins/MerkleMembershipsPlugin',
+        allInputs: [
+          {
+            treeRoot: Field.from(
+              '23677077440781146505135211739372132754346907641862154489298610120615142915141'
+            ),
+            leafIndex: BigInt(0),
+            secret: Field.from(0)
+          },
+          {
+            treeRoot: Field.from(
+              '18899731314113395294456531345036718829118004565425508785092877281253012411124'
+            ),
+            leafIndex: BigInt(2),
+            secret: Field.from(2)
+          }
+        ]
+      }
     }
-    // {
-    //   name: 'good memberships',
-    //   outcome: {
-    //     shouldSuccess: true
-    //   },
-    //   kind: 'merkleMemberships',
-    //   config: {
-    //     pluginUrl: 'http://127.0.0.1:3001/plugins/MerkleMembershipsPlugin',
-    //     allInputs: [
-    //       {
-    //         treeRoot: Field.from(
-    //           '23677077440781146505135211739372132754346907641862154489298610120615142915141'
-    //         ),
-    //         leafIndex: BigInt(0),
-    //         secret: Field.from(0)
-    //       },
-    //       {
-    //         treeRoot: Field.from(
-    //           '18899731314113395294456531345036718829118004565425508785092877281253012411124'
-    //         ),
-    //         leafIndex: BigInt(2),
-    //         secret: Field.from(2)
-    //       }
-    //     ]
-    //   }
-    // }
   ]
 });
