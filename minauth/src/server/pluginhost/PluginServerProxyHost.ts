33b262fd3fa2a5f238332222ef937ae8e469b981
adamczykm <m@adamczykm.me>
2024-02-27 18:27:58 +0100

    Add the missing stub..

--- minauth/src/server/pluginhost/PluginServerProxyHost.ts
+++ minauth/src/server/pluginhost/PluginServerProxyHost.ts
@@ -1,4 +1,33 @@
-import { IPluginHost } from '../pluginhost.js';
+import { TaskEither } from 'fp-ts/lib/TaskEither.js';
+import { Either } from 'fp-ts/lib/Either.js';
+import { FpInterfaceType, OutputValidity } from '../plugin-promise-api.js';
+import { IPluginHost, PMap } from '../pluginhost.js';
 
 
-export default class PluginServerProxyHost implements IPluginHost {}
+export default class PluginServerProxyHost implements IPluginHost<FpInterfaceType> {
+  __interface_tag: FpInterfaceType = 'fp';
+
+  verifyProofAndGetOutput(
+    inputs: { [plugin: string]: unknown }
+  ): TaskEither<string, PMap<Either<string, unknown>>>{
+    throw new Error('Method not implemented.');
+  }
+
+  checkOutputValidity(
+    output: { [plugin: string]: unknown }
+  ): TaskEither<string, PMap<OutputValidity>>{
+    throw new Error('Method not implemented.');
+  }
+
+  isReady(): TaskEither<string, boolean> {
+    throw new Error('Method not implemented.');
+  }
+
+  activePluginNames(): TaskEither<string, string[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  installCustomRoutes(app: any): TaskEither<string, void> {
+    throw new Error('Method not implemented.');
+  }
+}
